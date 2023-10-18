7f04bf2def481d1de588f4618417bbb274c2824b
Claudio Henrique de Oliveira Soares <87618859+ClaudioOliveira89@users.noreply.github.com>
2023-10-18 01:59:06 -0300

    Update Palindromo.py
    Corriginado os comentários do programa.

--- Desafios/Palindromo.py
+++ Desafios/Palindromo.py
@@ -2,17 +2,17 @@
 
 #Criando a função is_palindrome para o max_palindrome.
 
-def is_palindrome(str): #declrando a função
-    size = len(str) #calcula o tamanho da string.
-    if size == 0: #verifica se a string está vazia.
-        return False
+def is_palindrome(str): #declarando a função
+    size = len(str) #cálcula o tamanho da String.
+    if size == 0: #verifica se a String está vazia.
+        return False #se não retorna como falso.
     return all(str[i] == str[size - i - 1] for i in range(0, size // 2)) #verifica se é o palindromo.
 
 
 #Criando o Palindromo.
 #  
 def maior_palindromo(): #declarando a função.
-    max_palindrome = 0  #inicinando do zero.
+    max_palindrome = 0  #iniciando do zero.
     for i in range(999, 99, -1):  #o loop da sequência com três digitos.
         for j in range(i, 99, -1):  #o loop da sequência com dois digitos.
             product = i * j  #calculando o produto dos valores.
